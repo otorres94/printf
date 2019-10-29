578426096fc8329eed9db2dec098d14af823e0da
otorres94 <otorres1709@gmail.com>
2019-10-29 18:18:00 -0500

    Printf

--- holberton.h
+++ holberton.h
@@ -1,30 +1,32 @@
-#ifndef PRINT_H
-#define PRINT_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
 #include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <limits.h>
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
 
-int pr_char(va_list ap);
-
-int pr_str(va_list ap);
-
-int pr_int(va_list ap);
-
+int _putchar(char c);
 int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
-int pr_rot13(va_list ap);
-
-int pr_rev(va_list ap);
-
-int convert(va_list ap, int base);
-
-int convertU(va_list ap, int base);
-
-int non_prting(va_list ap);
-
-int pr_ptr(va_list ap);
-
-#endif
+#endif  /* _HOLBERTON_H */ 
